f8ed679f75f0bfce7e6b9b1c5ad7359607f44acc
samathaluca <samanthaluca@aol.com>
2020-04-17 08:43:55 +0000

    template files rendering with nav bar and base.html

---
+++ crib4tonight/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Crib4TonightConfig(AppConfig):
+    name = 'crib4tonight'
--- lazycamp/settings.py
+++ lazycamp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crib4tonight',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'lazycamp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+# 	os.path.join(BASE_DIR, "static")
+# ]
+
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'lazycamp/static')
+]
+

1cae1131a0261693d2ab293dfd2b189f07513968
samathaluca <samanthaluca@aol.com>
2020-04-23 14:17:48 +0000

    deploy repairs

--- lazycamp/settings.py
+++ lazycamp/settings.py
@@ -75,17 +75,17 @@ WSGI_APPLICATION = 'lazycamp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {      
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))  
-    } 
+# DATABASES = {      
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))  
+#     } 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # if "DATABASE_URL" in os.environ:
 #         DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}

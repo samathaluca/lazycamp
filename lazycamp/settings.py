de24373442ac30b698ba63643cf60fc37415838f
samathaluca <samanthaluca@aol.com>
2020-04-25 22:58:29 +0000

    whitenoise for staticfiles

--- lazycamp/settings.py
+++ lazycamp/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'lazycamp.urls'
@@ -146,3 +147,16 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'lazycamp/static')
 ]
 
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

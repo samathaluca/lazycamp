c17fbc1a190aefab6672b024b74eabf267fec5f6
samathaluca <samanthaluca@aol.com>
2020-04-29 10:38:57 +0000

    changed django secret key and put in env.py

--- campspot/tests.py
+++ campspot/tests.py
@@ -1,13 +1,17 @@
 from django.test import TestCase
-from campspot.models import campme
+# from campspot.models import campme
 
 # # Create your tests here.
-class campmeTests(TestCase):
+
+
+# class campmeTests(TestCase):
+
+
 #     """
 #     Here we'll define the tests that we'll run against our
 #     campspot model
 #     """
 
-    def test_str(self):
-        test_name = campme(name='A campspot')
-        self.assertEqual(str(test_name), 'A campspot')
+    # def test_str(self):
+    #     test_name = campme(name='A campspot')
+    #     self.assertEqual(str(test_name), 'A campspot')
--- lazycamp/settings.py
+++ lazycamp/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 
+if os.path.exists('env.py'):
+    import env
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,8 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7r3a^r%2=g!fo987tqpolxy!zf9-t%bparj$t$o$4f*jl^j*x&'
-
+# SECRET_KEY = '7r3a^r%2=g!fo987tqpolxy!zf9-t%bparj$t$o$4f*jl^j*x&'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

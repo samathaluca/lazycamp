aa209fb4d596a9b20532d60ead6295ed394f709d
samathaluca <samanthaluca@aol.com>
2020-04-30 15:24:45 +0000

    url home correction and styling index page

--- campspot/urls.py
+++ campspot/urls.py
@@ -3,7 +3,7 @@ from campspot import views
 
 urlpatterns = [
     
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('search/', views.search, name='search'),
     path('campspots/', views.campspots, name='campspots'),
     # path('campspot/<int:campme_id>', views.campspot, name='campspot'),
